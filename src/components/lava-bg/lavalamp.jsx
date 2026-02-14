ecb0207f6f377e3fc963004595a364e0b4f23d73
Antonio Laudani <antoniolaudani99@gmail.com>
2026-02-14 18:56:40 +0100

    fix

--- src/components/lava-bg/lavalamp.jsx
+++ src/components/lava-bg/lavalamp.jsx
@@ -16,23 +16,31 @@ const LavaLamp = () => {
 
   return (
     <>
-      {/* SVG Filter */}
-      <svg width="0" height="0" style={{ position: 'absolute' }}>
-        <filter id="metaball">
-          <feGaussianBlur in="SourceGraphic" stdDeviation="14" result="blur"/>
-          <feColorMatrix in="blur" mode="matrix"
-            values="
-              1 0 0 0 0
-              0 1 0 0 0
-              0 0 1 0 0
-              0 0 0 22 -8" />
-        </filter>
+      {/* SVG Filter - FIX SAFARI */}
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        version="1.1"
+        width="0" 
+        height="0" 
+        style={{ position: 'absolute', width: 0, height: 0 }}
+      >
+        <defs>
+          <filter id="metaball" x="-50%" y="-50%" width="200%" height="200%" colorInterpolationFilters="sRGB">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="14" result="blur"/>
+            <feColorMatrix 
+              in="blur" 
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 22 -8" 
+              result="goo"
+            />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
+          </filter>
+        </defs>
       </svg>
 
       {/* Lava Container */}
       <div className="lava-container">
         {!isMobile ? (
-          /* Desktop blobs */
           <>
             <div className="blob blob1"></div>
             <div className="blob blob2"></div>
@@ -43,7 +51,6 @@ const LavaLamp = () => {
             <div className="blob blob7"></div>
           </>
         ) : (
-          /* Mobile bubbles */
           <>
             <div className="bubble-mobile bubble-1"></div>
             <div className="bubble-mobile bubble-2"></div>

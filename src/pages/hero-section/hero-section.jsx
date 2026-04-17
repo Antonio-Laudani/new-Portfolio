deb87da394e389235133bc22e4d5c8d21da1681d
Antonio Laudani <antoniolaudani99@gmail.com>
2026-04-17 18:05:52 +0200

    fix

--- src/pages/hero-section/hero-section.jsx
+++ src/pages/hero-section/hero-section.jsx
@@ -1,7 +1,11 @@
 import Hero from '../../components/hero/hero';
 
 const HeroSection = () => {
-  return <Hero />;
+  return (
+    <div id="Hero">
+      <Hero />
+    </div>
+  );
 };
 
 export default HeroSection;
